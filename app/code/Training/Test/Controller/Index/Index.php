6f9c4d7110fede3c1b342713eafb89b0e6cd00ef
spotapenko <potapenko.stanislav@gmail.com>
2018-02-23 00:02:45 +0300

    Unit 2. Module 5. Task 2.5.1 Create a frontend controller that renders "HELLO WORLD"

---
+++ app/code/Training/Test/Controller/Index/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sfdev
+ * Date: 23.02.18
+ * Time: 0:00
+ */
+
+namespace Training\Test\Controller\Index;
+
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute() {
+        $this->getResponse()->appendBody("HELLO WORLD");
+    }
+}
