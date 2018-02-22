2ef620d22730181ae02f69d140fe75477ddfa159
spotapenko <potapenko.stanislav@gmail.com>
2018-02-22 08:19:27 +0300

    Unit Two.  Module 2. Request Flow Overview 2.2.1. observer debug body data

---
+++ app/code/Training/Test/Observer/LogPageOutput.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sfdev
+ * Date: 22.02.18
+ * Time: 7:55
+ */
+
+namespace Training\Test\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class LogPageOutput implements ObserverInterface
+{
+    /**
+     * @var null|\Psr\Log\LoggerInterface
+     */
+    protected $_logger = null;
+    /**
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public function __construct(\Psr\Log\LoggerInterface $logger)
+    {
+        $this->_logger = $logger;
+    }
+    /**
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return $this
+     *
+     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer) {
+        $response = $observer->getEvent()->getData('response');
+        $body = $response->getBody();
+        $this->_logger->addDebug("--------\n\n\n BODY \n\n\n ". $body);
+    }
+}
