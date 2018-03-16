d933161bc7aad0b64fc6244c5efa40aa9b05a7f5
spotapenko <potapenko.stanislav@gmail.com>
2018-03-17 00:00:13 +0300

    Unit 3 Module 8 3.8.2. Create a new controller action (ex: training_render/layout/onepage).

---
+++ app/code/Training/Render/Controller/Layout/Onepage.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Training\Render\Controller\Layout;
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Onepage extends \Magento\Framework\App\Action\Action {
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Customer order history
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Framework\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+
+        return $resultPage;
+    }
+} 
